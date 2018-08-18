d3980473a3de9d08ee3fbdbe16903c272222114b
Clay Dunston <dunstontc@gmail.com>
2018-08-17 19:48:01 -0500

    add gohtml

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ const json5 = require('gulp-json5-to-json');
 
 gulp.task('default', () => { });
 
-gulp.task('compile-json', () => {
-  gulp.src('./src/**/*.json5')
+gulp.task('compile-go', () => {
+  gulp.src('./src/go/*.json5')
     .pipe(merge({
       fileName: "go.tmLanguage.json",
       json5: true,
@@ -16,6 +16,18 @@ gulp.task('compile-json', () => {
     .pipe(gulp.dest('./syntaxes'));
 });
 
+gulp.task('compile-gohtml', () => {
+  gulp.src('./src/gohtml/*.json5')
+    .pipe(merge({
+      fileName: "gohtml.tmLanguage.json",
+      json5: true,
+    }))
+    .pipe(json5({
+      beautify: true,
+    }))
+    .pipe(gulp.dest('./syntaxes'));
+});
+
 gulp.task('watch', () => {
-  gulp.watch('./src/**/*.json5', [ 'compile-json' ]);
+  gulp.watch('./src/**/*.json5', [ 'compile-go', 'compile-gohtml' ]);
 });
